22a9982039a2396fae0cbadb278e274165520f9b
Cesar Zulueta <cesarzulueta12@gmail.com>
2022-04-15 12:23:52 -0700

    updated resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,7 +2,7 @@ import React from "react"
 // import { Document, Page, View, Text } from "@react-pdf/renderer"
 import { Document, Page} from "react-pdf/dist/esm/entry.webpack";
 // import { Page } from "react-pdf"
-import resume from "../static/resume.pdf"
+import resume from "../static/NewResume.pdf"
 import "../css/resume.css"
 
 
