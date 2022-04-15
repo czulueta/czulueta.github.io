1ebaf6827e9727ed96266778eba1732bed7571c0
Cesar Zulueta <cesarzulueta12@gmail.com>
2022-04-15 11:07:19 -0700

    deleted browserRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { render } from "react-dom"
 import React from "react"
-import { BrowserRouter as Router, HashRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import App from "./App.js"
 
 
